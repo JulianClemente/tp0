7c34324b5010c5081d02e5d067cf7bb0748da3fb
Julian Clemente <jclemente@frba.utn.edu.ar>
2026-08-20 07:20:49 +0000

    Arreglo a servidores

--- server/src/utils.c
+++ server/src/utils.c
@@ -20,10 +20,12 @@ int iniciar_servidor(void)
 
 	// Creamos el socket de escucha del servidor
 
+	socket_servidor = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
 	// Asociamos el socket a un puerto
-
+	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
 	// Escuchamos las conexiones entrantes
-
+	listen(socket_servidor, 1);
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
 
@@ -35,7 +37,8 @@ int esperar_cliente(int socket_servidor)
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	accept(socket_servidor, NULL, NULL);
+	int socket_cliente = socket(hints);
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
